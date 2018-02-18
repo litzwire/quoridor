d6dc4636a52759b385b70afd503758c3f608650a
Colby Johnson <colby.johnson25@gmail.com>
2018-02-17 23:16:09 -0500

    added column support

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -9,12 +9,26 @@ const displayThisWorks = function () {
 const createRows = function (number) {
   const rows = []
   for (let i = 0; i < number; i++) {
+    const cells = createCells(number, i)
     const row = {
-      row_index: i
+      row_index: i,
+      cells: cells
     }
     rows.push(row)
   }
   return rows
+}
+
+const createCells = function (number, row_index) {
+  const cells = []
+  for (let i = 0; i < number; i++) {
+    const cell = {
+      row_index: row_index,
+      cell_index: i
+    }
+    cells.push(cell)
+  }
+  return cells
 }
 
 const createGameBoard = function (number) {
